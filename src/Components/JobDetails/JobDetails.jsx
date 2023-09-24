f517fbe78e091706dbb106debc0a66e265adbbe7
IJE2704 <mh1669101@gmail.com>
2023-09-24 16:29:24 +0600

    job details component comple

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { JobDetailsContext } from '../JobCart';
 import { useLoaderData, useParams } from 'react-router-dom';
 import PageBanner from '../PageBanner/PageBanner';
+import Footer from '../Footer/Footer';
 
 const JobDetails = () => {
     const jobs = useLoaderData();
     const { id } = useParams();
     const intId = parseInt(id);
     const job = jobs.find(job => job.id === intId);
-    const {salary,jobName,phone,location,email,details,responsibilities,education,experience} = job;
+    const { salary, jobName, phone, location, email, details, responsibilities, education, experience } = job;
     const pageName = 'Job Details';
     // console.log(job);
     return (
@@ -16,27 +17,32 @@ const JobDetails = () => {
             <PageBanner
                 pageName={pageName}
             ></PageBanner>
-            <div className='flex'>
+            <div className='container mx-auto flex my-32 '>
                 <div className='flex-1'>
                     <p className='mt-6'><span className='font-bold'>Job Details:</span> {details}</p>
                     <p className='mt-6'><span className='font-bold'>Job Responsibilities:</span> {responsibilities}</p>
                     <p className='mt-6'><span className='font-bold'>Education:</span> {education}</p>
                     <p className='mt-6'><span className='font-bold'>Experience: </span>{experience}</p>
                 </div>
-                <div className='flex-3'>
-                    <div>
-                    <h1>Job Details</h1>
-                    <p><span>Salary:</span> {salary}</p>
-                    <p><span>Job Title:</span> {jobName}</p>
-                    <h1>Contact Information</h1>
-                    <p><span>Phone:</span> {phone}</p>
-                    <p><span>Email:</span> {email}</p>
+                <div className='flex-2 '>
+                    <div className='bg4 p-7 mb-6 rounded'>
+                        <h1 className='font-extrabold text-xl'>Job Details</h1>
+                        <div className='w-full border-t bg3 border-sky-600 my-6'>
+                        </div>
+                        <p className='mb-4'><span className='font-bold'>Salary:</span> {salary}</p>
+                        <p><span className='font-bold'>Job Title:</span> {jobName}</p>
+                        <h1  className='font-extrabold text-xl mt-8'>Contact Information</h1>
+                        <div className='w-full border-t bg3 border-sky-600 my-6'>
+                        </div>
+                        <p className='mb-4'><span className='font-bold'>Phone:</span> {phone}</p>
+                        <p><span className='font-bold'>Email:</span> {email}</p>
                     </div>
                     <div>
-                        <button></button>
+                        <button className='btn w-full'>Apply Now</button>
                     </div>
                 </div>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
